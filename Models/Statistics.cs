97fe404422f2cfc14d01fb5bf6b7e09086736e27
Ivelin Kirilov <ivelinkv@gmail.com>
2015-05-31 11:17:18 +0300

    Update to the statistics table

--- Models/Statistics.cs
+++ Models/Statistics.cs
@@ -14,7 +14,7 @@
 
         public int MostPointsOfWord { get ; set; }
 
-        public int UserId { get; set; } 
+        public string UserId { get; set; }
 
         public virtual User User { get; set; }
     }
