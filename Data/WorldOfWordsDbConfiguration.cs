99e9913ecfaa194d9125f5f46aea0003ab251734
Ivelin Kirilov <ivelinkv@gmail.com>
2015-12-11 13:04:44 +0200

    added SqlAzureExecutionStrategy to fix apphb errors

---
+++ Data/WorldOfWordsDbConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace Data
+{
+    using System.Data.Entity;
+    using System.Data.Entity.SqlServer;
+
+    public class WorldOfWordsDbConfiguration : DbConfiguration
+    {
+        public WorldOfWordsDbConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
